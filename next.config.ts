d9b09361e6937fb9bd204f9e7e655b706f18fe31
Atef MASMOUDI <masatef@gmail.com>
2026-05-01 15:27:39 +0100

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api",
+        destination: "http://127.0.0.1:8000/api",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
